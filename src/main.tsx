4045bb5b6e4044517f6bd742d8753e8fd5a8b968
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-12-24 21:43:48 +0530

    24-12-2022

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@fontsource/fira-code";
+import "./assets/index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/styles.tsx
@@ -0,0 +1,9 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  fontFira: {
+    fontFamily: "'Fira Code', monospace",
+  },
+});
+
+export default useStyles;
